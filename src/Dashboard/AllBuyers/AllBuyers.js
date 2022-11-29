7a092d0d9559fc4bab2f0512fd5a31a47c941a57
Asif Farhan <asifhossain953@gmail.com>
2022-11-29 17:31:36 +0600

    map data

--- src/Dashboard/AllBuyers/AllBuyers.js
+++ src/Dashboard/AllBuyers/AllBuyers.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Container, Row} from 'react-bootstrap';
+import { Container, Row } from 'react-bootstrap';
 import { useLoaderData } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -7,11 +7,28 @@ import AllBuyersCard from './AllBuyersCard/AllBuyersCard';
 
 const AllBuyers = () => {
     const data = useLoaderData()
-    const [curentBuyers,setCurrentBuyers]=useState(data);
+    const [currentBuyers, setCurrentBuyers] = useState(data);
+    const handleDelete = (_id) => {
+        const proceed = window.confirm('Are you sure to delete this seller ?');
+        if (proceed) {
+            fetch(`http://localhost:5000/users/seller/${_id}`, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data)
+                    if (data.deletedCount > 0) {
+                        toast("Deleted Successfully");
+                        const remaining = currentBuyers.filter(seller => seller._id !== _id)
+                        setCurrentBuyers(remaining);
+                    }
+                })
+        }
+    }
     return (
         <Container>
             <Row className='g-3'>
-                <AllBuyersCard></AllBuyersCard>
+                {currentBuyers.map(data=><AllBuyersCard handleDelete={handleDelete} data={data} key={data._id}></AllBuyersCard>)}
             </Row>
             <ToastContainer />
         </Container>
