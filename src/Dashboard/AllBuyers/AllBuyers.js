71416c644d01e9d6202aa4c0c06d5757c212a4a6
Asif Farhan <asifhossain953@gmail.com>
2022-11-29 16:59:54 +0600

    buyer added

--- src/Dashboard/AllBuyers/AllBuyers.js
+++ src/Dashboard/AllBuyers/AllBuyers.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 
 const AllBuyers = () => {
     return (
-        <div>
-            this is all buyer
-        </div>
+        <Container>
+
+        </Container>
     );
 };
 
