46e89e72b42ef6e3697cbe89af22c27cc5f63844
Asif Farhan <asifhossain953@gmail.com>
2022-11-29 17:08:48 +0600

    all buyers card component added to all buyers

--- src/Dashboard/AllBuyers/AllBuyers.js
+++ src/Dashboard/AllBuyers/AllBuyers.js
@@ -3,13 +3,14 @@ import { Container, Row} from 'react-bootstrap';
 import { useLoaderData } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import AllBuyersCard from './AllBuyersCard/AllBuyersCard';
 
 const AllBuyers = () => {
     const data = useLoaderData()
     return (
         <Container>
             <Row className='g-3'>
-                
+                <AllBuyersCard></AllBuyersCard>
             </Row>
             <ToastContainer />
         </Container>
