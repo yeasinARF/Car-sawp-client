2677d7e5711e7e4eba629e26516f02a24cba9723
Asif Farhan <asifhossain953@gmail.com>
2022-11-29 17:07:14 +0600

    buyers card added

--- src/Dashboard/AllBuyers/AllBuyers.js
+++ src/Dashboard/AllBuyers/AllBuyers.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Row} from 'react-bootstrap';
 import { useLoaderData } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const AllBuyers = () => {
-    const data=useLoaderData()
+    const data = useLoaderData()
     return (
         <Container>
-
+            <Row className='g-3'>
+                
+            </Row>
+            <ToastContainer />
         </Container>
     );
 };
---
+++ src/Dashboard/AllBuyers/AllBuyersCard/AllBuyersCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AllBuyersCard = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default AllBuyersCard;
